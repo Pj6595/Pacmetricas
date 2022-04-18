ef966476c47d76c7b543efb69c45e017b9ac94a4
Pj <47722275+Pj6595@users.noreply.github.com>
2022-04-18 21:18:25 +0200

    barras

--- Runtime/Persistence.cs
+++ Runtime/Persistence.cs
@@ -102,7 +102,7 @@ namespace Pacmetricas_G01{
 				}
 
 				string buffer = serializer.GetFullSerialization();
-				string filePath = path + "gs_" + gameSession + serializer.GetSerializationFormat();
+				string filePath = path + "\gs_" + gameSession + serializer.GetSerializationFormat();
 
 				FileStream fs;
 				if(File.Exists(filePath))
